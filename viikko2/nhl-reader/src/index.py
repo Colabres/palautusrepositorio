95bfa2ea94453a4308efc9d7ebc876485498effc
Colabres <pavel.allen@gmail.com>
2023-11-13 12:03:07 +0200

    tehtava 12 done

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,31 +1,56 @@
 import requests
 from player import Player
 
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    #print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        #print(player_dict)
-        players.append(player)
-
-    print("Oliot:")
-    nat = input("Give nationality")
-    print(f"Players from {nat}")
-    playerList=[]
     for player in players:
-        if player.nationality == nat.upper():
-            playerList.append(player)
-    sortedPlayer=sorted(playerList,key=lambda x: x.points)
-    sortedPlayer.reverse()
-    for player in sortedPlayer:
         print(player)
+
+class PlayerReader():
+    
+    def __init__(self, url):
+        self.url=url
+        self.players=[]
+
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+                
+
+        for player_dict in response:
+            player = Player(player_dict)
+            self.players.append(player)
+
+        return self.players
+
+class PlayerStats():
+
+
+    def __init__(self,reader):
+        self.reader=reader
+
+    def top_scorers_by_nationality(self, nat):
+
+
+        print(f"Players from {nat}")
+        playerList=[]
+        allPlayers=self.reader.get_players()
+        for player in allPlayers:
+            if player.nationality == nat.upper():
+                playerList.append(player)
+        sortedPlayer=sorted(playerList,key=lambda x: x.points)
+        sortedPlayer.reverse()
+        
+        return sortedPlayer
+        
+        #for player in sortedPlayer:
+            #print(player)
+
 
 
 if __name__ == "__main__":
