8b36e3f4e5b893b07d1ec770c7774ad4baf7d4ac
Colabres <pavel.allen@gmail.com>
2023-12-11 15:39:41 +0200

    pull.md created

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,17 +1,32 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+    query = QueryBuilder()
+
+    matcher = (
+    query
+    .oneOf(
+      query.playsIn("PHI")
+          .hasAtLeast(10, "assists")
+          .hasFewerThan(5, "goals")
+          .build(),
+      query.playsIn("EDM")
+          .hasAtLeast(50, "points")
+          .build()
     )
+    .build()
+)
+    #matcher = query.oneOf(m1, m2).build()
+    #query = QueryBuilder()
+
+    #matcher = query.oneOf("f","f").build()
+
 
     for player in stats.matches(matcher):
         print(player)
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,3 +1,23 @@
+
+
+# class OneOf:
+#     def __init__(self, m1, m2):
+#         self._m1 = m1
+#         self._m2 = m2
+#         print(f"OneOf initialized with m1:{m1} , m2: ")
+        
+#     def test(self, player):
+#         print("Testing OneOf")
+#         return self._m1.test(player) or self._m2.test(player)
+
+class All:
+    
+
+    def test(self, player):
+
+        return True
+
+
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
@@ -8,6 +28,27 @@ class And:
                 return False
 
         return True
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
+
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def test(self, player):
+        return not self._matcher.test(player)
+
 
 
 class PlaysIn:
@@ -27,3 +68,42 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._matcher=Not(HasAtLeast(value,attr))
+
+    def test(self, player):
+        
+        return self._matcher.test(player)
+
+
+
+class QueryBuilder:
+    def __init__(self,query=All()):
+        #self._matchers = []
+        self._query=query
+
+    def oneOf(self,m1,m2):
+        #self._matchers.append(Or(m1,m2))
+        #self._matchers.append(OneOf(m1,m2))
+        #print(self._matchers)
+
+        return QueryBuilder(Or(m1,m2))
+
+
+    def playsIn(self, team):
+        #self._matchers.append(PlaysIn(team))
+        return QueryBuilder(And(self._query,PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        #self._matchers.append(HasAtLeast(value, attr))
+        return QueryBuilder(And(self._query,HasAtLeast(value,attr)))
+
+    def hasFewerThan(self, value, attr):
+        #self._matchers.append(HasFewerThan(value, attr))
+        return QueryBuilder(And(self._query,HasFewerThan(value,attr)))
+
+    
+    def build(self):
+        return self._query
